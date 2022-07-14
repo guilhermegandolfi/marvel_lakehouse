b36e85583491f3074d5691e3c9740821a58953dd
guilhermegandolfi <ggandolfi@DESKTOP-AT7N4V7.localdomain>
2022-07-14 08:17:03 -0300

    firt commit

---
+++ raw_data/ingestion_full.py
@@ -0,0 +1,26 @@
+import boto3
+import os 
+from datetime import datetime
+from tqdm import tqdm
+
+class IngestionRawData:
+
+
+    def __init__(self, bucket, nam_path):
+        self.bucket=bucket
+        self.nam_path=nam_path
+
+dt= datetime.now()
+dt=dt.strftime('year=%y/month=%m/day=%d')
+
+
+s3_client = boto3.client('s3')
+for root, dirs, files in os.walk('../data_files/fhv'):
+    for name in tqdm(files):
+        if name.endswith('.parquet'):
+            nam_file=os.path.join(root,name)
+            nam_object=nam_file.split('/')
+            nam_object=os.path.join('ny_taxis',nam_object[-2],dt,nam_object[-1]) 
+            response = s3_client.upload_file(nam_file, 'thanos-raw-533506519916-dev', nam_object)
+
+
