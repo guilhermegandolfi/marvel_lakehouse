f39cd90074d641e96412f08a21cdd55e5650bed8
guilhermegandolfi <ggandolfi@DESKTOP-AT7N4V7.localdomain>
2022-08-02 06:38:32 -0300

    improvement in the code

--- raw_data/ingestion_full.py
+++ raw_data/ingestion_full.py
@@ -5,22 +5,29 @@ from tqdm import tqdm
 
 class IngestionRawData:
 
-
     def __init__(self, bucket, nam_path):
         self.bucket=bucket
         self.nam_path=nam_path
 
-dt= datetime.now()
-dt=dt.strftime('year=%y/month=%m/day=%d')
+    @staticmethod
+    def get_date_now():
+        dt= datetime.now()
+        dt=dt.strftime('year=%y/month=%m/day=%d')
+        return dt
 
+    def ingestion_files(self, dt, prefix):
+        s3_client = boto3.client('s3')
+        for root, dirs, files in os.walk(self.nam_path):
+            for name in tqdm(files):
+                if name.endswith('.parquet'):
+                    nam_file=os.path.join(root,name)
+                    nam_object=nam_file.split('/')
+                    nam_object=os.path.join(prefix,nam_object[-2],dt,nam_object[-1]) 
+                    response = s3_client.upload_file(nam_file, self.bucket, nam_object)
 
-s3_client = boto3.client('s3')
-for root, dirs, files in os.walk('../data_files'):
-    for name in tqdm(files):
-        if name.endswith('.parquet'):
-            nam_file=os.path.join(root,name)
-            nam_object=nam_file.split('/')
-            nam_object=os.path.join('ny_taxis',nam_object[-2],dt,nam_object[-1]) 
-            response = s3_client.upload_file(nam_file, 'thanos-lakehouse-raw-data-dev', nam_object)
-
+if __name__ == '__main__':
+    ingestion = IngestionRawData('thanos-lakehouse-raw-data-dev','../data_files')
+    dt_now=ingestion.get_date_now()
+    ingestion.ingestion_files(dt_now,'ny_taxis')
+    
 
